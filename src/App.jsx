663ba2ebea40d893b7765a7e7dfa5644e26b39b6
zahra mortazavi <zahramortazavi23@gmail.com>
2025-01-06 13:01:38 +0330

    create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
+import Layout from "./layouts/Layout"
+import Home from "./pages/Home";
 
 function App() {
 
   return (
     <>
-
+      <Layout>
+        <Home />
+      </Layout>
     </>
   )
 }
